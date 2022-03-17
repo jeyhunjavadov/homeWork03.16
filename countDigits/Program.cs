b84e30689bcd67cd16e456d397bbec4e66b3bab4
Ceyhun Cavadov <ceyhunccavadov@Ceyhuns-MacBook-Pro.local>
2022-03-17 10:05:33 +0400

    Uploading Homework

---
+++ countDigits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace countDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 567895;
+            int count = 0;
+
+            if (num == 0)
+            {
+                count++;
+            }
+            else
+            {
+                while (num != 0)
+                {
+                    num -= num % 10;
+                    num /= 10;
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
